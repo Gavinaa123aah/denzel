df3334dde5214b7fbf92b456706352dee7b2d304
gavin <gavinaa123aah@163.com>
2019-03-20 11:04:31 +0100

    import movies_file.json into mogodb

--- sandbox.js
+++ sandbox.js
@@ -2,16 +2,20 @@
 const imdb = require('./src/imdb');
 const DENZEL_IMDB_ID = 'nm0000243';
 
+
 async function sandbox (actor) {
   try {
     console.log(`📽️  fetching filmography of ${actor}...`);
     const movies = await imdb(actor);
     const awesome = movies.filter(movie => movie.metascore >= 77);
+    
+    // console.log(movies)
 
-    console.log(`🍿 ${movies.length} movies found.`);
-    console.log(JSON.stringify(movies, null, 2));
-    console.log(`🥇 ${awesome.length} awesome movies found.`);
-    console.log(JSON.stringify(awesome, null, 2));
+
+    // console.log(`🍿 ${movies.length} movies found.`);
+    // console.log(JSON.stringify(movies, null, 2));
+    // console.log(`🥇 ${awesome.length} awesome movies found.`);
+    // console.log(JSON.stringify(awesome, null, 2));
     process.exit(0);
   } catch (e) {
     console.error(e);
